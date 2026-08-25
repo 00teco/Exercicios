b3ee37503465ebd71769241bd04c56f86abf1bc2
00teco <joao.barao.nascimento@escola.pr.gov.br>
2026-08-24 21:09:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Atividades Práticas carregadas com sucesso.");
+    console.log("Atividades Práticas carregadas.");
 });
