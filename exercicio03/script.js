3e8303bcddeb3cf83f21d4549aa99b3cddeaf8b1
00teco <joao.barao.nascimento@escola.pr.gov.br>
2026-08-24 21:45:50 -0300

    Update script.js

--- exercicio03/script.js
+++ exercicio03/script.js
@@ -1,14 +1,13 @@
-function aumentarTexto() {
-    document.getElementById("mensagem")
-        .style.fontSize = "30px";
-}
-
-function mudarFonte() {
-    document.getElementById("mensagem")
-        .style.fontFamily = "Courier New";
-}
-
-function negritoTexto() {
-    document.getElementById("mensagem")
-        .style.fontWeight = "bold";
-}
+const mensagem = document.querySelector("#mensagem");
+ 
+document.querySelector("#aumentar").addEventListener("click", () => {
+    mensagem.style.fontSize = "30px";
+});
+ 
+document.querySelector("#mudarFonte").addEventListener("click", () => {
+    mensagem.style.fontFamily = "Courier New, monospace";
+});
+ 
+document.querySelector("#negrito").addEventListener("click", () => {
+    mensagem.style.fontWeight = "bold";
+});
